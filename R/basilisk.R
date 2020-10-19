8119a9d047bcc60a135fc10742be32e053cef02e
beng-phrasia <52316962+beng-phrasia@users.noreply.github.com>
2020-10-19 17:36:51 +0800

    Update basilisk.R
    Ensure leiden can be used with basilisk

--- R/basilisk.R
+++ R/basilisk.R
@@ -4,6 +4,8 @@ python_env <- BasiliskEnvironment(
     packages = c(
       "opentsne==0.4.3",
       "scikit-learn=0.23.1",
+      "leidenalg=0.8.2",
+      "python-igraph=0.8.3",
       if (basilisk.utils::isWindows()) "scipy=1.5.0" else "scipy=1.5.1",
       "numpy=1.19.0"
     )
